330608604d2d63c4731d22d1333b7147db69b437
sok.heng <sok.heng@wingbank.com.kh>
2026-08-13 18:47:15 +0700

    feat: finish uplaod to rustfs by presigned

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider"
 
 const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
 
@@ -22,11 +23,20 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
+    <html suppressHydrationWarning
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-mono", jetbrainsMono.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+        </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
-import Image from "next/image";
+"use client";
+
 import {Button} from "@/components/ui/button";
-import {Card, CardHeader, CardContent, CardTitle, CardAction, CardFooter} from "@/components/ui/card";
+import {Card, CardHeader, CardContent, CardTitle, CardFooter} from "@/components/ui/card";
 import {Label} from "@/components/ui/label";
 import {Input} from "@/components/ui/input";
 import {Field, FieldDescription, FieldLabel} from "@/components/ui/field";
+import {Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow} from "@/components/ui/table";
+import {ButtonGroup} from "@/components/ui/button-group";
+import {useRouter} from "next/navigation";
+
+const users = [
+    {
+        id: 1,
+        name: "su",
+        key: 123111
+    },
+    {
+        id: 2,
+        name: "li",
+        key: 123222
+    },
+    {
+        id: 3,
+        name: "Mei",
+        key: 123333
+    }
+];
 
 export default function Home() {
     return (
@@ -14,33 +36,40 @@ export default function Home() {
 }
 
 function CardUpload() {
+    const router = useRouter();
+
+    function handleClickAdd() {
+        router.push("/upload");
+    }
+
     return (
         <Card className="w-full max-w-sm">
             <CardHeader className={"justify-center"}>
-                <CardTitle>RustFS Stream Drop </CardTitle>
+                <CardTitle>User Profiles</CardTitle>
             </CardHeader>
             <CardContent>
-                <form>
-                    <div className="flex flex-col gap-6">
-                        <div className="grid gap-2">
-                            <Field>
-                                <FieldLabel htmlFor="picture">File</FieldLabel>
-                                <Input id="file" type="file"/>
-                                <FieldDescription>Expect: JPG, PNG, JPEG only. </FieldDescription>
-                            </Field>
-                        </div>
-
-                        <Button type="submit" className="w-full">
-                            Upload
-                        </Button>
-                    </div>
-                </form>
+                <Table className="my-3">
+                    <TableCaption>
+                        <Button className="w-full rounded-none" variant={"default"} onClick={handleClickAdd} >Add</Button>
+                    </TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="text-center">Id</TableHead>
+                            <TableHead className="text-center">Name</TableHead>
+                            <TableHead className="text-center">Key</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {users.map((user) => (
+                            <TableRow key={user.id}>
+                                <TableCell className="text-center">{user.id}</TableCell>
+                                <TableCell className="text-center">{user.name}</TableCell>
+                                <TableCell className="text-center">{user.key}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
             </CardContent>
-            <CardFooter className="flex-col gap-2">
-                <Label>Resutl</Label>
-                <Label>=== Key ===</Label>
-                <Label>=== Link ===</Label>
-            </CardFooter>
         </Card>
     );
 }
